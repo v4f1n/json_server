aa41cec0f9f5996e31cd4d14a91e3edab068f843
v4f1n <ebsbsbso@gmail.com>
2023-12-10 16:55:55 +0300

    fix cors

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+server.use((req, res, next) => {
   setTimeout(next, 1000);
 });
 
-server.use(cors());
+server.use(cors({}));
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
